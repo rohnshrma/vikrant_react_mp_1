df9a284405f022be564d5c527ca3234a9a518626
rohnshrma <rohn11shrma@gmail.com>
2022-12-21 22:19:10 +0530

    adding meals (without form)

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -1,13 +1,19 @@
+import { Fragment } from "react"
 import classes from "./Header.module.css"
 import HeaderCartButton from "./HeaderCartButton"
-
+import mealsImage from "../../assets/meals.jpg"
 
 const Header = props => {
     return (
-    <header className={classes.header} >
-        <h1>Tomato</h1>
-        <HeaderCartButton />
-    </header>)
+        <Fragment>
+            <header className={classes.header} >
+                <h1>Tomato</h1>
+                <HeaderCartButton />
+            </header>
+            <div className={classes["main-image"]}>
+                <img src={mealsImage} alt="meals"/>
+            </div>
+        </Fragment>)
 }
 
 
---
+++ src/Components/Meals/MealItem/MealItem.js
@@ -0,0 +1,18 @@
+import classes from "./MealItem.module.css"
+
+import React from 'react'
+
+const MealItem = (props)=>{
+  return (
+<li className={classes.meal}>
+    <div>
+        <h3>{props.name}</h3>
+        <div className={classes.description}>{props.description}</div>
+        <div className={classes.price}>{props.price}</div>
+    </div>
+    <div></div>
+</li>
+    )
+}
+
+export default MealItem

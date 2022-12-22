e7d5cbc6b9f399ce71dbdf87cd58ad110d839b1f
rohnshrma <rohn11shrma@gmail.com>
2022-12-22 22:02:07 +0530

    meaItemForm done

--- src/Components/Meals/MealItem/MealItem.js
+++ src/Components/Meals/MealItem/MealItem.js
@@ -11,7 +11,7 @@ const MealItem = (props)=>{
         <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{props.price}</div>
     </div>
-    <div><MealItemForm/></div>
+    <div><MealItemForm id={props.id} /></div>
 </li>
     )
 }
--- src/Components/Meals/MealItem/MealItemForm.js
+++ src/Components/Meals/MealItem/MealItemForm.js
@@ -5,7 +5,7 @@ const MealItemForm = (props) => {
     return (
         <form className={classes.form}>
             <Input label="Amount :" input={{
-                id:props.id,
+                id:"amount_" + props.id,
                 type:"number",
                 min:"1",
                 max:"5",
